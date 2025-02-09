58623be11fbcc101c33325fbe687da3e11ebd6f9
Cezary24 <cezarypastor555@gmail.com>
2025-02-09 10:07:34 +0100

    remove CORS_ALLOWED_ORIGINS

--- investpolis/investpolis/settings.py
+++ investpolis/investpolis/settings.py
@@ -106,9 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-]
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
